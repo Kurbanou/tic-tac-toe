96ab5b8033f82c76b856e8961ecc2fa8da01ee28
Farid Kurbanou <kurbanou.faridun@gmail.com>
2022-02-25 00:05:01 +0300

    feat:add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const main = document.querySelector('main')
 const start = document.querySelector('a')
-startGame()
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function createApp(){
     const app = document.createElement('section')
@@ -27,46 +29,57 @@ function createApp(){
         game.append(ceil[i])}
         setTimeout( function(){sss()}, i*120)
     }
-    const message = document.createElement('div')
-    message.classList.add('message')
-    message.innerHTML = 'Ходит игрок'
-    table.append(message)
-    const countGames =document.createElement('div')
-    countGames.classList.add('countGames')
-    countGames.innerHTML = 'Сыгранно игр : 0'
-    table.append(countGames)
-    const score =document.createElement('div')
-    score.classList.add('score')
-    table.append(score)
-    const player =document.createElement('div')
-    player.classList.add('player')
-    player.innerHTML = 'Имя игрока'
-    score.append(player)
-    const draw = document.createElement('div')
-    draw.classList.add('draw')
-    score.append(draw)
-    const comp = document.createElement('div')
-    comp.classList.add('comp')
-    score.append(comp)
-    const playerScore =document.createElement('div')
-    playerScore.classList.add('playerScore')
-    score.append(playerScore)
-    const drawScore = document.createElement('div')
-    drawScore.classList.add('drawScore')
-    score.append(drawScore)
-    const compScore = document.createElement('div')
-    compScore.classList.add('compScore')
-    score.append(compScore)
-    const resetScore = document.createElement('div')
-    resetScore.classList.add('resetScore')
-    score.append(resetScore)
+
+    function createTable(){
+        const message = document.createElement('div')
+        message.classList.add('message')
+        message.innerHTML = 'Ходит игрок'
+        table.append(message)
+        const countGames =document.createElement('div')
+        countGames.classList.add('countGames')
+        countGames.innerHTML = 'Сыгранно игр : 0'
+        table.append(countGames)
+        const score =document.createElement('div')
+        score.classList.add('score')
+        table.append(score)
+        const player =document.createElement('div')
+        player.classList.add('player')
+        player.innerHTML = 'Имя игрока'
+        score.append(player)
+        const draw = document.createElement('div')
+        draw.classList.add('draw')
+        draw.innerHTML = 'Ничьи'
+        score.append(draw)
+        const comp = document.createElement('div')
+        comp.classList.add('comp')
+        comp.innerHTML = 'Компьютер'
+        score.append(comp)
+        const playerScore =document.createElement('div')
+        playerScore.classList.add('playerScore')
+        playerScore.innerHTML = '0'
+        score.append(playerScore)
+        const drawScore = document.createElement('div')
+        drawScore.classList.add('drawScore')
+        drawScore.innerHTML = '0'
+        score.append(drawScore)
+        const compScore = document.createElement('div')
+        compScore.classList.add('compScore')
+        compScore.innerHTML = '0'
+        score.append(compScore)
+        const resetScore = document.createElement('div')
+        resetScore.classList.add('resetScore')
+        resetScore.innerHTML = 'Сбросить счет'
+        table.append(resetScore)
+    }
+    setTimeout( function(){
+        createTable()
+        table.classList.add('show')
+    }, 1500)
+
 }
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
+start.addEventListener('click', createApp)
 
-
-
-function startGame(){
-    start.addEventListener('click', createApp)
-}
